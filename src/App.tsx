c4e5839cf0b63b52bfebebeee882d228a589866a
Mona A <mona@DN0a249516.SUNet>
2023-11-12 12:58:37 -0800

    jpmc_task2

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import DataStreamer, { ServerRespond } from './DataStreamer';
 import Graph from './Graph';
 import './App.css';
+import { ServerResponse } from 'http';
 
 /**
  * State declaration for <App />
  */
 interface IState {
   data: ServerRespond[],
+  showGraph: boolean,  // property to ctrl when the graph is displayed
 }
 
 /**
@@ -22,6 +24,7 @@ class App extends Component<{}, IState> {
       // data saves the server responds.
       // We use this state to parse data down to the child element (Graph) as element property
       data: [],
+      showGraph: false,  // start the app without a graph
     };
   }
 
@@ -29,19 +32,29 @@ class App extends Component<{}, IState> {
    * Render Graph react component with state.data parse as property data
    */
   renderGraph() {
+    if (this.state.showGraph) {  // check showGraph property first 
     return (<Graph data={this.state.data}/>)
+    }
   }
 
   /**
    * Get new data from server and update the state with the new data
    */
   getDataFromServer() {
-    DataStreamer.getData((serverResponds: ServerRespond[]) => {
-      // Update the state by creating a new array of data that consists of
-      // Previous data in the state and the new data from server
-      this.setState({ data: [...this.state.data, ...serverResponds] });
-    });
-  }
+    let x = 0;  // start the counter
+    const interval = setInterval(() => {
+        DataStreamer.getData((serverResponds: ServerRespond[]) => {
+            this.setState({
+                data: serverResponds,
+                showGraph: true
+            });
+        });
+        x++;
+        if (x > 1000) {  // Check if the counter exceeds 1000 iterations.
+            clearInterval(interval);
+        }
+    }, 100);  // Set the fetch to occur every 100 milliseconds.
+}
 
   /**
    * Render the App react component
--- src/Graph.tsx
+++ src/Graph.tsx
@@ -14,7 +14,7 @@ interface IProps {
  * Perspective library adds load to HTMLElement prototype.
  * This interface acts as a wrapper for Typescript compiler.
  */
-interface PerspectiveViewerElement {
+interface PerspectiveViewerElement extends HTMLElement {
   load: (table: Table) => void,
 }
 
@@ -32,7 +32,7 @@ class Graph extends Component<IProps, {}> {
 
   componentDidMount() {
     // Get element to attach the table from the DOM.
-    const elem: PerspectiveViewerElement = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
+    const elem = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
 
     const schema = {
       stock: 'string',
@@ -46,9 +46,13 @@ class Graph extends Component<IProps, {}> {
     }
     if (this.table) {
       // Load the `table` in the `<perspective-viewer>` DOM reference.
-
-      // Add more Perspective configurations here.
       elem.load(this.table);
+      elem.setAttribute('view', 'y_line');  // continuous line graph 
+      elem.setAttribute('column-pivots', '["stock"]');  // distinguish stock ABC from DEF
+      elem.setAttribute('row_pivots', '["timestamp"]');  // takes care of our x-axis
+      elem.setAttribute('columns', '["top_ask_price"]');  // focus on a top_ask_price along y-axis
+      elem.setAttribute('aggregates',
+                        '{"stock":"distinct_count", "top_ask_price":"avg","top_bid_price":"avg", "timestamp":"distinct_count"}');
     }
   }
 
